593ac5a746444fa5f0fa24baaf8c5fc8349d9f4b
Voxold <bilalajaha@gmail.com>
2023-10-25 09:06:08 -0700

    Updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,12 +3,33 @@
 #include <string.h>
 #include <stdio.h>
 
+const listin_t **_r(const listin_t **list, size_t size, const listin_t)
+{
+    const listin_t **newlist;
+    size_t i;
+
+    newlist = malloc(size * sizeof(listin_t *));
+    if (newlist == NULL)
+    {
+        free(list);
+        exit(98);
+    }
+    for (i=0; i < size - 1;i++)
+        newlist[i] = list[i];
+    newlist[i] = new;
+    free(list);
+    return (newlist);
+
+}
+
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
  *
  * Return: number of nodes in the list
+ *
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
