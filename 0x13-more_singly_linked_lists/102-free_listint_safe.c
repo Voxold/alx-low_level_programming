9bdd31dd645b22873cf04cce57916d2ab0d07426
Voxold <bilalajaha@gmail.com>
2023-10-25 11:37:11 -0700

    Updated

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,7 +19,7 @@ size_t free_listint_safe(listint_t **h);
 size_t looped_listint_count(listint_t *head)
 {
 	listint_t *tortoise, *hare;
-	size_t nodes = 1;
+	size_t nodes = 0;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
@@ -27,33 +27,34 @@ size_t looped_listint_count(listint_t *head)
 	tortoise = head->next;
 	hare = (head->next)->next;
 
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
+	while (hare != NULL && hare->next != NULL)
+    {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
 
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
+        if (tortoise == hare)
+        {
+            // Detected a loop, initialize tortoise and count nodes
+            tortoise = head;
+            while (tortoise != hare)
+            {
+                nodes++;
+                tortoise = tortoise->next;
+                hare = hare->next;
+            }
 
-			return (nodes);
-		}
+            tortoise = tortoise->next;
+            while (tortoise != hare)
+            {
+                nodes++;
+                tortoise = tortoise->next;
+            }
 
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
+            return nodes;
+        }
+    }
 
-	return (0);
+    return nodes; // No loop detected, return the count
 }
 
 /**
