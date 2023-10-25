fb388fc9e22f3cb390b09f354a26b0bf30fe9c83
Voxold <bilalajaha@gmail.com>
2023-10-25 09:20:46 -0700

    Updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "lists.h"
 
+size_t _free_listint_safe(listint_t **);
+
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * _add_node - Add a node at the beginning of a list
  *
- * Return: number of nodes in the list
+ * @head: A pointer to the first element of a list
+ * @n: The number to store in the new node
  *
+ * Return: The address of the new node
  */
-
-size_t print_listint_safe(const listint_t *head)
+listint_t *_add_node(listint_t **head, int n)
 {
-	size_t num = 0;
-	long int diff;
+	listint_t *tmp;
 
-	while (head)
-	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
+	tmp = malloc(sizeof(*tmp));
+	if (!tmp)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
 
-	return (num);
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *node1;
+    listint_t *node2;
+    listint_t *node3;
+    listint_t *node4;
+    listint_t *node5;
+	size_t n;
+
+	head = NULL;
+    /* 1 -> 4 -> 3 -> 2 -> 5 -> (3) */
+	node5 = _add_node(&head, 5);
+    node4 = _add_node(&head, 4);
+    node3 = _add_node(&head, 3);
+    node2 = _add_node(&head, 2);
+    node1 = _add_node(&head, 1);
+
+    node1->next = node4;
+    node4->next = node3;
+    node3->next = node2;
+    node2->next = node5;
+    node5->next = node3;
+    
+	n = print_listint_safe(head);
+	printf("%lu\n", n);
+	_free_listint_safe(&head);
+	return (0);
 }
